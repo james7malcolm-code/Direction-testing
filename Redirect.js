838b85c8d1f2ea0e2151f3c7845153cb77cd1893
james7malcolm-code <james7malcolm@gmail.com>
2025-07-26 21:28:38 -0400

    Create Redirect.js

---
+++ Redirect.js
@@ -0,0 +1,6 @@
+export default function handler(req, res) {
+  // Replace with the URL you want to redirect to
+  const targetUrl = "interpals08.firebaseio.com";
+
+  res.redirect(302, targetUrl);
+}
