c8d8e01d1a45e2ddfb1694356b5a6b1346f6ad78
james7malcolm-code <james7malcolm@gmail.com>
2025-08-01 13:05:09 -0400

    Update Redirect.js

--- api/Redirect.js
+++ api/Redirect.js
@@ -4,3 +4,23 @@ export default function handler(req, res) {
 
   res.redirect(302, targetUrl);
 }
+const https = require('https');
+
+const options = {
+  host: 'TARGET_IP_OR_DOMAIN',  // Where you're connecting
+  port: 443,
+  method: 'GET',
+  path: '/',
+  headers: {
+    'Host': 'TARGET_HOST',      // Optional: spoof Host header too
+  },
+  servername: 'x.com'           // <-- This forces SNI to be x.com
+};
+
+const req = https.request(options, (res) => {
+  console.log('Status:', res.statusCode);
+  res.on('data', (chunk) => process.stdout.write(chunk));
+});
+
+req.on('error', (err) => console.error('Error:', err));
+req.end();
