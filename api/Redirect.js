b36d07ec8330ffd4d5dae9bfb269c561497d1c89
james7malcolm-code <james7malcolm@gmail.com>
2025-08-09 21:18:44 -0400

    Update Redirect.js

--- api/Redirect.js
+++ api/Redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   // Replace with the URL you want to redirect to
-  const targetUrl = "http://176.74.228";
+  const targetUrl = "http://192.168.1.254";
 
   res.redirect(302, targetUrl);
 }
