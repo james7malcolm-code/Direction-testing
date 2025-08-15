bd93220a125a8f6abbdc09f1fd9924d829f73b59
james7malcolm-code <james7malcolm@gmail.com>
2025-08-14 23:13:16 -0400

    Update Redirect.js

--- api/Redirect.js
+++ api/Redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   // Replace with the URL you want to redirect to
-  const targetUrl = "http://x.com";
+  const targetUrl = "http://x.Y29t";
 
   res.redirect(302, targetUrl);
 }
